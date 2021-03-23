b6b94d4cd5720141a292abb680f4925383beed01
Rylan Wassem <dryland28@gmail.com>
2021-03-23 16:49:32 -0400

    changed stuff

--- Program.cs
+++ Program.cs
@@ -1,34 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace SquareRoot
 {
     class Program
     {
         static void Main(string[] args) {
 
-            Console.WriteLine("\n\nEnter Q to quit.");
+            System.Console.WriteLine("\n\nEnter Q to quit.");
             double squareroot;
             bool cont = true;
 
             while (cont) {
-                Console.Write("\nEnter a number to get its squareroot: ");
-                string input = Console.ReadLine();
+                System.Console.Write("\nEnter a number to calculate its squareroot: ");
+                string input = System.Console.ReadLine();
                 if (input == "Q")
                     cont = false;
                 else if (double.TryParse(input, out double inumb)) {
                     if (inumb < 0) {
                         inumb *= -1;
-                        squareroot = Math.Sqrt(inumb);
-                        Console.WriteLine($"The squareroot of the number {inumb:F3} is {squareroot:F3}i");
+                        squareroot = System.Math.Sqrt(inumb);
+                        System.Console.WriteLine($"The squareroot of the number {inumb:F3} is {squareroot:F3}i");
                     } else {
-                        squareroot = Math.Sqrt(inumb);
-                        Console.WriteLine($"The squareroot of the number {inumb:F3} is {squareroot:F3} ");
+                        squareroot = System.Math.Sqrt(inumb);
+                        System.Console.WriteLine($"The squareroot of the number {inumb:F3} is {squareroot:F3} ");
                     }
-                } else Console.WriteLine("\nINVALID INPUT!!!");
+                } else System.Console.WriteLine("\nINVALID INPUT!!!");
 
             }
         }
